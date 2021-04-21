a037c66adfe3429129cb84a1eed386a485fd7b25
JohnHajjar <john.hajjar@isae.edu.lb>
2021-04-22 00:58:56 +0300

    lil update

--- mainpage.php
+++ mainpage.php
@@ -1,9 +1,25 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+</head>
+<body>
 
-<?php
-echo 'testmain';
-?>
+<img src="..\web3x\imgs\img-welcome.jpg" /> 
 
 
+<div id="div-bar">
 
+    <div class="button" id="button-6">
+        <div id="spin"></div>
+        <a class="a-button"href="\..\web3x\index.php">About Us</a>
+    </div>
+
+    <div class="button" id="button-5">
+        <div id="translate"></div>
+        <a class="a-button" href="#">Let's Go!</a>
+    </div>
+
+</div>
+
+</body>
 </html>
