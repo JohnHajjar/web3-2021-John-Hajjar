40adcbc3e1b839d8fbca195681d03617acc5d8ee
JohnHajjar <john.hajjar@isae.edu.lb>
2021-06-11 13:46:10 +0300

    mainpage update

--- mainpage.php
+++ mainpage.php
@@ -4,21 +4,45 @@
 </head>
 <body>
 
-<img src="..\web3x\imgs\img-welcome.jpg" /> 
 
-
-<div id="div-bar">
-
-    <div class="button" id="button-6">
-        <div id="spin"></div>
-        <a class="a-button"href="\..\web3x\index.php">About Us</a>
+<div class="img1">
+    <div>
+        <img style="float:right;" src="imgs/opengif.gif" widht=50% height=50%>
     </div>
+</div>
 
-    <div class="button" id="button-5">
-        <div id="translate"></div>
-        <a class="a-button" href="#">Let's Go!</a>
+<div style="color: #777;background-color:white; text-align:center; padding:20px 50px; text-align: justify;">
+    <h3 style="text-align:center;">WELCOME TO JEWXRLY</h3>
+    <p style="text-align:center;">
+        Jewxrly is an online store that specializes in a range of minimalistic, fashion-forward, <br>
+        and expensive-looking jewerly with an affordable price for both men, women and couples.
+    </p>
+    <div id="div-bar">
+
+            <div class="button" id="button-6">
+            <div id="spin"></div>
+            <a class="a-button"href="\..\web3x\index.php">More about us !</a>
+            </div>
+
+            <div class="button" id="button-5">
+                <div id="translate"></div>
+                <a class="a-button" href="#">Shop now!</a>
+            </div>
+    </div> 
+    
+
+</div>
+
+<div class="img2">
+    <div>
+        
     </div>
+</div>
 
+<div class="img3">
+    <div style="background-color:black;">
+        <p>ccccccccc</p>
+    </div>
 </div>
 
 </body>
