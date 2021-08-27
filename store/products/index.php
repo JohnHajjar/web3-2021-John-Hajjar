b91c2f1f51a6d7c28b3fa09d7e49d5dca51f56b6
JohnHajjar <john.hajjar@isae.edu.lb>
2021-08-27 19:11:41 +0300

    products page 1.0

--- store/products/index.php
+++ store/products/index.php
@@ -10,8 +10,12 @@
 
         <link rel="stylesheet" type="text/css" href="../../stylesheet.css"/>
         <link href="http://fonts.cdnfonts.com/css/brandon-grotesque-regular" rel="stylesheet">
+
     </head>
-    <body>
+    <body class="body-productinfo">
+        <?php if (empty($_POST["prodid"]) || !isset($_POST["prodid"]))
+                { header('Location: ../');  }
+         ?>
     <?php include '../../db.php'; ?>
         <?php include '../../upbar.php'; ?>   
 
@@ -74,25 +78,43 @@
             </div>
         </section>
 
+
         <section id="prodetails-right">
-            <div class="rrrr">
-                <h2> <?php echo $productName; ?> </h2>
-                <h4 align=center> <?php echo $productType; ?> </h4>
+            <div>
+                <h1 align=center> <?php echo $productName; ?> </h1>
+                <h4 align=center> 
+                    <?php echo $gendermf.'  '; ?> &nbsp; - &nbsp; <?php echo ' '.$productType; ?> 
+                </h4>
                 
                 <div class="rating-stars">
-                        <span class="rrrr"> Product rating : <?php echo $ratings; ?> </span>
+                        <div class="star-ratings">
+                        <?php $ratingperc = (100*$ratings)/5; ?>
+                        <div class="fill-ratings" style="width: <?php echo $ratingperc; ?>%;">
+                            <span>★★★★★</span>
+                        </div>
+                        <div class="empty-ratings">
+                            <span>★★★★★</span>
+                        </div>
+                        </div>
+                    (<?php echo $ratings; ?>)
                 </div>
                 <div class="prodprice">
-                   <h3> <?php echo '$'.$price; ?> </h3>
+                   <h3 align=center> <?php echo '$'.$price; ?> </h3>
                 </div>
-                <hr>
-                <table>
+            <hr>
+                <table align=center class="table-productpage">
                     <tr>
                         <td>
                             In stock 
                         </td>
                         <td>
-                            asd <!--add small green tick if stock >=0 and gray if not -->
+                            <?php if($instock > 3){ $instockcolor = 'green';}
+                                  else if ($instock > 0 && $instock < 3)
+                                        { $instockcolor = 'orange';}
+                                  else {$instockcolor = 'red';}
+                                ?>
+                           <div class="color-instock" style="background-color:<?php echo $instockcolor ?>;">
+                           </div> 
                         </td>
                         <td>
                             <?php echo $instock.' left.'; ?>
@@ -103,22 +125,75 @@
                             Color 
                         </td>
                         <td>
-                            asd<!-- add circle with color inside easy border radius back-color -->
+                            <div class="color-productpage" style="background-color:<?php echo $color ?>">
+                            </div>
                         </td>
                         <td>
                             <?php echo $color; ?>
                         </td>
                     </tr>
                 </table>
+          <br><br>
+            <div class="addtocart-div">
+                <button class="addtocart-btn"> ADD TO CART </button>
+            </div>
         <br><br>
-                    <h3> Description & Details. </h3>
-    
-    
+        <hr>
+                    <h3 align=center> Description & Details. </h3>
+                    <span id="span-more" onclick="showhidefunction()" align=center>+</span>
+                     <div id="moredetails-productpage">
+                        <h4 align=center>
+                            Description:
+                        </h4>
+                        <p align=center> 
+                            <?php echo $description; ?>
+                        </p>
+                        <hr>
+                        <h4 align=center>
+                            Dimensions
+                        </h4>
+                        <p align=center>
+                            <?php echo $dimensions; ?>
+                        </p>
+                        <hr>
+                        <h4 align=center>
+                            Weight
+                        </h4>
+                        <p align=center>
+                            <?php echo $weight; ?>
+                        </p>
+                        <hr>
+                     </div>
+
         </div> 
     
        </section>
 
         <script>
+            function showhidefunction () {
+                if (document.getElementById("moredetails-productpage").style.display == "block")
+                {
+                    document.getElementById("span-more").innerHTML = "+";
+                    document.getElementById("moredetails-productpage").style.display = "none";
+                } else {
+                    document.getElementById("span-more").innerHTML = "-";
+                    document.getElementById("moredetails-productpage").style.display = "block";
+                }
+        }
+
+            /*------------------*/
+                                
+                $(document).ready(function() {
+                // Gets the span width of the filled-ratings span
+                // this will be the same for each rating
+                var star_rating_width = $('.fill-ratings span').width();
+                // Sets the container of the ratings to span width
+                // thus the percentages in mobile will never be wrong
+                $('.star-ratings').width(star_rating_width);
+                });
+            
+            /*------------------*/
+
                 const imgs = document.querySelectorAll('.img-select a');
                 const imgBtns = [...imgs];
                 let imgId = 1;
